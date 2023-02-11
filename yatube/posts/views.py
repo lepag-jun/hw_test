ee7f876f28f484b21ba3bca69d0b5786db5b506a
lepag-jun <almazbr@gmail.com>
2023-02-11 10:33:07 +0300

    add html, models, admin

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
 
 
-def index(request):    
-    template = 'posts/index.html'
-    return render(request, template) 
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    return render(request, template) 
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
