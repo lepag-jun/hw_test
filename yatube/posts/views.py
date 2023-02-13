d09d7ba173df9adb5df044e662114d1b10974bc1
lepag-jun <almazbr@gmail.com>
2023-02-13 23:56:09 +0300

    add post_create

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 
 from .models import Post, Group, User
+from .forms import PostForm
 
 
 def index(request):
@@ -19,10 +20,13 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
     }
 
     return render(request, 'posts/group_list.html', context)
@@ -52,3 +56,38 @@ def post_detail(request, post_id):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def post_create(request):
+    template = 'posts/create_post.html'
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', request.user)
+
+    return render(request, template, {'form': form})
+
+
+@login_required
+def post_edit(request, post_id):
+    template = 'posts/create_post.html'
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author != request.user:
+        return redirect("posts:post_detail", post_id)
+
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post.id)
+
+    return render(
+        request,
+        template,
+        {'form': form,
+         'is_edit': True,
+         'post_id': post.id,
+         },
+    )
